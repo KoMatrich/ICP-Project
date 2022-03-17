8d84ebe37a785d49e057d4e44009e7a391b9539c
KoMatrich <kocichmartin@gmail.com>
2022-03-17 14:42:11 +0100

    Stack syntax
    Broken on line by line check

--- highlighter.cpp
+++ highlighter.cpp
@@ -1,83 +1,105 @@
 #include "highlighter.h"
+#include <QDebug>
 
 Highlighter::Highlighter(QTextDocument *parent)
     : QSyntaxHighlighter(parent)
 {
-    //key words
-    keywordFormat.setForeground(Qt::darkBlue);
-    keywordFormat.setFontWeight(QFont::Bold);
+    err.setBackground(Qt::red);
+    after_err.setForeground(Qt::darkGray);
 
-    QStringList keywordPatterns;
-    keywordPatterns << "^participant" << "^'participant" << "^title"
-                    << "^class"       << "^bactivate"   << "^note"
-                    << "^deactivate"  << "^activate";
+    Rule r;
+    r.start = QRegExp("@startuml");
+    r.end   = QRegExp("@enduml");
+    r.format.setFontWeight(QFont::Bold);
 
-    HighlightingRule rule;
-    foreach (const QString &pattern, keywordPatterns) {
-        rule.pattern = QRegExp(pattern);
-        rule.format = keywordFormat;
-        highlightingRules.append(rule);
+    syntax.append(r);
+}
+
+int Highlighter::match(const QString &text, int &offset, RuleSet &parts, Rule &current)
+{
+    //find start of sub block
+    for(int i = 0; i < parts.length();i++){
+        Rule part = parts.at(i);
+        QRegExp start = part.start;
+        int match_i = start.indexIn(text,offset);
+        if(match_i == offset){
+            setFormat(offset,start.matchedLength(),part.format);
+            offset += start.matchedLength();
+            setCurrentBlockState(1);
+            stack.append(i);
+            return 0;
+        }
     }
 
-    //@key words
-    classFormat.setFontWeight(QFont::Bold);
-    classFormat.setForeground(Qt::darkMagenta);
-    rule.pattern = QRegExp("^@[A-Za-z]+");
-    rule.format = classFormat;
-    highlightingRules.append(rule);
+    //find end of actual block
+    QRegExp end = current.end;
+    if(!end.isEmpty()){
+        int match_i = end.indexIn(text,offset);
+        if(match_i == offset){
+            setFormat(offset,end.matchedLength(),current.format);
+            offset += end.matchedLength();
+            setCurrentBlockState(1);
+            stack.pop_back();
+            return 1;
+        }
+    }
 
-    quotationFormat.setForeground(Qt::darkGreen);
-    rule.pattern = QRegExp("\"[^\"]*\"");
-    rule.format = quotationFormat;
-    highlightingRules.append(rule);
+    //nothing has been found
+    setFormat(offset,text.length(),err);
+    return -1;
+}
 
-    functionFormat.setFontItalic(true);
-    functionFormat.setForeground(Qt::blue);
-    rule.pattern = QRegExp("^[A-Za-z0-9_]+ ?(\\-\\>|\\-\\-\\>) ?[A-Za-z0-9_]+:");
-    rule.format = functionFormat;
-    highlightingRules.append(rule);
+void Highlighter::find(const QString &text, int &offset)
+{
+    if(syntax.length()==0){
+        setCurrentBlockState(-3);
+        return;
+    }
 
-    singleLineCommentFormat.setForeground(Qt::gray);
-    rule.pattern = QRegExp("^' [^\n]*");
-    rule.format = singleLineCommentFormat;
-    highlightingRules.append(rule);
+    //empty line check
+    if(text.isEmpty()){
+        setCurrentBlockState(1);
+        return;
+    }
 
-    multiLineCommentFormat.setForeground(Qt::gray);
-    commentStartExpression = QRegExp("^/' ");
-    commentEndExpression = QRegExp(" '/$");
+    RuleSet parts = syntax;
+    Rule current;
+    if(stack.length()==0){
+        //stack empty       => not in body
+    }else{
+        //stack not empty   => in body
+        int stack_l = stack.length();
+        int i = 0;
+        while(stack_l > i){
+            current = parts.at(stack.at(i));
+            parts   = current.parts;
+            i++;
+        }
+    }
+
+    match(text,offset,parts,current);
 }
 
 void Highlighter::highlightBlock(const QString &text)
 {
-    //simple one line rules
-    foreach (const HighlightingRule &rule, highlightingRules) {
-        QRegExp expression(rule.pattern);
-        int index = expression.indexIn(text);
-        while (index >= 0) {
-            int length = expression.matchedLength();
-            setFormat(index, length, rule.format);
-            index = expression.indexIn(text, index + length);
-        }
-    }
-
-    //simple multi line rule
-    setCurrentBlockState(0);
-
-    int startIndex = 0;
-    if (previousBlockState() != 1)
-        startIndex = commentStartExpression.indexIn(text);
-
-    while (startIndex >= 0) {
-        int endIndex = commentEndExpression.indexIn(text, startIndex);
-        int commentLength;
-        if (endIndex == -1) {
-            setCurrentBlockState(1);
-            commentLength = text.length() - startIndex;
-        } else {
-            commentLength = endIndex - startIndex
-                            + commentEndExpression.matchedLength();
-        }
-        setFormat(startIndex, commentLength, multiLineCommentFormat);
-        startIndex = commentStartExpression.indexIn(text, startIndex + commentLength);
+    setCurrentBlockState(-2);
+    switch(previousBlockState()){
+    case -3:
+        //internal error
+        setCurrentBlockState(-3);
+        break;
+    case -2:
+        //error ocured in syntax last check
+        setFormat(0,text.length(),after_err);
+        setCurrentBlockState(-2);
+        break;
+    case -1:
+        //start of file
+        stack.clear();
+    case 1:
+        //normal operation
+        int start = 0;
+        find(text,start);
+        break;
     }
 }
