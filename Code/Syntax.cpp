d95b65fc1c0a28a2da00b425012ac87c211daeb5
KoMatrich <kocichmartin@gmail.com>
2022-04-10 11:01:45 +0200

    More syntax + example

--- Code/Syntax.cpp
+++ Code/Syntax.cpp
@@ -33,27 +33,6 @@ QRegExp inline Start(const QString input){
 RuleSet const Syntax::genRules(){
     RuleSet syntax;
 
-    Rule name;
-    name.start = Start("\\s+[A-Za-z0-9_]+");
-    name.format.setForeground(Qt::darkGreen);
-
-    Rule string_content;
-    string_content.start = Start("[A-Za-z0-9_\\s]+");
-    string_content.format.setForeground(Qt::darkGreen);
-
-    Rule string;
-    string.start = Start("\\s+\"");
-    string.type  = MULTI_LINE;
-    string.end   = End("\"");
-    string.format.setForeground(Qt::green);
-    string.parts.append(string_content);
-
-    Rule type;
-    type.start = Start("\\s+(int|string|float)");
-    type.format.setFontWeight(QFont::Bold);
-    type.parts.append(name);
-    type.parts.append(string);
-
     //uml body
     Rule uml;
     uml.start = Start("^@startuml$");
@@ -66,6 +45,15 @@ RuleSet const Syntax::genRules(){
     clas.start = Start("^\\s*class\\s+[A-Za-z0-9_]+$");
     clas.type  = MULTI_LINE;
     clas.format.setFontWeight(QFont::Bold);
+
+    Rule name;
+    name.start = Start("\\s*_?[A-Za-z][A-Za-z0-9_]*");
+    name.format.setForeground(Qt::darkGreen);
+
+    Rule type;
+    type.start = Start("\\s*(int|string|float)");
+    type.format.setFontWeight(QFont::Bold);
+    type.parts.append(name);
 
     //class atr
     Rule class_atr;
