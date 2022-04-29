3da84eb059667fc8f374d70e9ef7723ceab72300
KoMatrich <kocichmartin@gmail.com>
2022-04-29 21:23:59 +0200

    Grid snaping

--- include/Graph/ERDItem.h
+++ include/Graph/ERDItem.h
@@ -31,12 +31,17 @@ public:
     void paint(QPainter* painter,
                const QStyleOptionGraphicsItem* option,
                QWidget* widget) override;
-private:
-    const int OFFSET{ 10 };
-    const QPoint POFFSET{ OFFSET,OFFSET };
+protected:
+    QVariant itemChange(GraphicsItemChange change,
+                        const QVariant& value);
 
-    const int RADIUS{ 5 };
+private:
+    const int GRID_S = 12;
+    const int OFFSET{ GRID_S / 2 };
+    const int RADIUS{ GRID_S };
     const int SEPARATOR_H{ 3 };
+
+    const QPoint POFFSET{ OFFSET,OFFSET };
 
     //automatic var
     QLinearGradient fill;
@@ -51,7 +56,7 @@ private:
     QSize size;
     constexpr QSize Size() { return QSize{ this->Width(), this->Height() }; }
     QSize rsize;
-    constexpr QSize RSize() { return QSize{ size.width() + POFFSET.x() * 2, size.height() + POFFSET.x() * 2 }; }
+    constexpr QSize RSize() { return QSize{ qCeil(qreal(size.width() + POFFSET.x() * 2) / GRID_S) * GRID_S, qCeil(qreal(size.height() + POFFSET.x() * 2) / GRID_S) * GRID_S }; }
     //used by paintSeparator
     constexpr QPoint* points();
 
