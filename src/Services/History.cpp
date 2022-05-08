2f011f22c1484b448a5fd04b1d28b609ac48410b
KoMatrich <kocichmartin@gmail.com>
2022-05-08 11:24:18 +0200

    Undo optimization

--- src/Services/History.cpp
+++ src/Services/History.cpp
@@ -10,15 +10,13 @@ void HistoryService::takeHistorySnapshot()
     HistoryService& instance = HistoryService::getInstance();
 
     // if empty, always push
-    if (instance.history.empty())
-    {
+    if (instance.history.empty()) {
         instance.history.push_back(instance.code->document()->toRawText());
         return;
     }
 
     /// push up to max size, delete front if needed
-    if (instance.history.size() > MAX_HIST_SIZE)
-    {
+    if (instance.history.size() > MAX_HIST_SIZE) {
         instance.history.erase(instance.history.begin());
     }
     instance.history.push_back(instance.code->document()->toRawText());
@@ -28,25 +26,24 @@ void HistoryService::restoreHistorySnapshot()
 {
     HistoryService& instance = HistoryService::getInstance();
     QString snapshot;
-    QString current;
+    QString current = instance.code->document()->toRawText();;
     bool changed = false;
-    while (!changed)
-    {
+    while (!changed) {
         if (instance.history.empty()) {
             return;
         }
 
         snapshot = instance.history.back();
-        current = instance.code->document()->toRawText();
-        if (current != snapshot)
-        {
-            changed = true;
-        }
         instance.history.pop_back();
-        instance.code->document()->setPlainText(snapshot);
-    }
-    instance.history.push_back(snapshot);
 
+        if (current != snapshot) {
+            changed = true;
+        } else {
+            current = snapshot;
+        }
+    }
+    instance.code->document()->setPlainText(snapshot);
+    instance.history.push_back(snapshot);
 }
 
 void HistoryService::setCodeWindow(QTextEdit* c)
